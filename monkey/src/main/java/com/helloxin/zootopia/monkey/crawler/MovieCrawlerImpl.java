1d3bf461ed3ca550430d11ad0c5ae7d440bde6ab
叶信节 <yexinjie@souche.com>
2019-01-31 17:13:20 +0800

    ADD 今天就先这样子吧

--- monkey/src/main/java/com/helloxin/zootopia/monkey/crawler/MovieCrawlerImpl.java
+++ monkey/src/main/java/com/helloxin/zootopia/monkey/crawler/MovieCrawlerImpl.java
@@ -2,6 +2,8 @@ package com.helloxin.zootopia.monkey.crawler;
 
 
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,15 +15,25 @@ import java.util.Date;
  * Created by nandiexin on 2019/1/31.
  */
 @Component
-public class MovieCrawlerImpl implements MovieCrawler {
-
+public class MovieCrawlerImpl implements MovieCrawler  {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    static WebDriver driver = null;
+
+    static{
+        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+        driver =new ChromeDriver();
+    }
 
 
     @Override
     public void crawlerTodayMovieInfo(){
+
         logger.info("crawlerTodayMovieInfo start time ={}",new Date());
+
+        driver.get("http://www.baidu.com");
+        driver.close();
 
 
 
