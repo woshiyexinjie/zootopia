ef567d151b285407e4478f6839e925bcc0e370bf
叶信节 <yexinjie@souche.com>
2019-02-02 21:42:21 +0800

    ADD 增加一些第三方工具包

--- monkey/src/main/java/com/helloxin/zootopia/monkey/crawler/MovieCrawlerImpl.java
+++ monkey/src/main/java/com/helloxin/zootopia/monkey/crawler/MovieCrawlerImpl.java
@@ -2,13 +2,18 @@ package com.helloxin.zootopia.monkey.crawler;
 
 
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -31,13 +36,25 @@ public class MovieCrawlerImpl implements MovieCrawler  {
     public void crawlerTodayMovieInfo(){
 
         logger.info("crawlerTodayMovieInfo start time ={}",new Date());
+        // 这里想看看电影网站
+        driver.get("https://movie.douban.com/cinema/nowplaying/hangzhou/");
 
-        driver.get("http://www.baidu.com");
+        List<WebElement> items = driver.findElements(By.className("list-item"));
+        if(!CollectionUtils.isEmpty(items)){
+            logger.info("要处理的数据 size={}",items.size());
+
+        }else{
+            logger.info("抓取到的豆瓣电影上映数据为空");
+        }
+
+
         driver.close();
 
 
 
 
     }
+    
+
 
 }
