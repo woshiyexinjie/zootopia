6b6d743cf57b5b1527b4388a50f8c02602000b3d
叶信节 <yexinjie@souche.com>
2019-02-27 14:21:22 +0800

    MODIFY  chrome option

--- monkey/src/main/java/com/helloxin/zootopia/monkey/crawler/MovieCrawlerImpl.java
+++ monkey/src/main/java/com/helloxin/zootopia/monkey/crawler/MovieCrawlerImpl.java
@@ -49,8 +49,9 @@ public class MovieCrawlerImpl implements MovieCrawler  {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
         options.addArguments("--disable-gpu");
-        options.addArguments("user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'");
+        options.addArguments("--disable-dev-shm-usage");
 
         DesiredCapabilities cap = DesiredCapabilities.chrome();
         cap.setCapability(ChromeOptions.CAPABILITY, options);
