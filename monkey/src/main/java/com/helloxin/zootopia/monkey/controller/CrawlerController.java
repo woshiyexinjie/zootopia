91e15799113b3b1e3c0aa1ffc1e230722a0ed2b6
叶信节 <yexinjie@souche.com>
2019-02-27 12:47:42 +0800

    ADD A http method to crawler douban movie

---
+++ monkey/src/main/java/com/helloxin/zootopia/monkey/controller/CrawlerController.java
@@ -0,0 +1,35 @@
+package com.helloxin.zootopia.monkey.controller;
+
+import com.helloxin.zootopia.monkey.crawler.MovieCrawler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.Instant;
+
+/**
+ * Created by nandiexin on 2019/2/27.
+ */
+@RestController
+public class CrawlerController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private MovieCrawler movieCrawler;
+
+    @RequestMapping("crawlerDouban")
+    public void crawlerDoubanMovie() {
+
+        Instant begin = Instant.now();
+        logger.info("reportCurrentTime start time={}", begin);
+
+        movieCrawler.crawlerTodayMovieInfo();
+
+        Instant end = Instant.now();
+        logger.info("reportCurrentTime end time={},duration={}", end, Duration.between(begin,end).getNano()/1000);
+    }
+}
