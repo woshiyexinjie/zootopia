28a0b3a408bb876d4157202935d78e148c0e150e
叶信节 <yexinjie@souche.com>
2019-09-23 19:01:59 +0800

    ADD 将cat-lottery项目加入goose-queue 项目

--- cat_lottery/src/main/java/com/helloxin/zootopia/cat/lottery/Application.java
+++ cat_lottery/src/main/java/com/helloxin/zootopia/cat/lottery/Application.java
@@ -3,12 +3,14 @@ package com.helloxin.zootopia.cat.lottery;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
 @MapperScan("com.helloxin.zootopia.mouse.dao")
+@ComponentScan(basePackages = {"com.helloxin.zootopia.**"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
